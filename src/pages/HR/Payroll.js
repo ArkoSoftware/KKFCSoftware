409b15f91e2ae438a2a7a10b4b39d1f01f3cdda1
indrajith76 <indrajithgoswami@gmail.com>
2023-03-01 10:03:45 +0600

    update

--- src/pages/HR/Payroll.js
+++ src/pages/HR/Payroll.js
@@ -36,25 +36,50 @@ const Payroll = () => {
         <table className="w-full">
           <thead className="bg-[#EAEAEA] rounded">
             <tr>
-              <th className="font-normal py-2 text-gray-500 rounded-tl-xl rounded-bl-xl">
+              <th className="font-normal text-xs py-2 text-gray-500 rounded-tl-xl rounded-bl-xl">
                 NAME
               </th>
-              <th className="font-normal py-2 text-gray-500">LEAVE DAY</th>
-              <th className=" rounded-tr-xl  rounded-br-xl">WORKING DAY</th>
-              <th className=" rounded-tr-xl  rounded-br-xl">OVERTIME</th>
-              <th className=" rounded-tr-xl  rounded-br-xl">
-                OVERTIME PAY SALARY
+              <th className="font-normal text-xs py-2 text-gray-500">LEAVE DAY</th>
+              <th className="font-normal text-xs py-2 text-gray-500">WORKING DAY</th>
+              <th className="font-normal text-xs py-2 text-gray-500">OVERTIME</th>
+              <th className="font-normal text-xs py-2 text-gray-500">
+                OVERTIME
+                <br />
+                PAY SALARY
               </th>
-              <th className=" rounded-tr-xl  rounded-br-xl">TOTAL SALARY</th>
+              <th className="font-normal text-xs py-2 text-gray-500">
+                TOTAL
+                <br />
+                SALARY
+              </th>
+              <th className="font-normal text-xs py-2 text-gray-500">
+                GROSS
+                <br />
+                SALARY
+              </th>
+              <th className="font-normal text-xs py-2 text-gray-500">
+                NET SALARY LESS
+                <br />
+                ADV AND TDS
+              </th>
+              <th className="font-normal text-xs py-2 text-gray-500 rounded-tr-xl rounded-br-xl">
+                TDS DEDUCTION 1%
+                <br /> FROM GS
+              </th>
             </tr>
           </thead>
           <tbody>
             {[1, 2, 3].map((data) => (
               <tr className="border-b-2">
                 <td className="text-center text-sm py-3">Andie Bell</td>
-                <td className="text-center text-sm py-3">
-                  Civil Mall, Kathmandu
-                </td>
+                <td className="text-center text-sm py-3">2</td>
+                <td className="text-center text-sm py-3">28</td>
+                <td className="text-center text-sm py-3">5</td>
+                <td className="text-center text-sm py-3">$80</td>
+                <td className="text-center text-sm py-3">$100</td>
+                <td className="text-center text-sm py-3">$180</td>
+                <td className="text-center text-sm py-3">$0</td>
+                <td className="text-center text-sm py-3">$0</td>
               </tr>
             ))}
           </tbody>
